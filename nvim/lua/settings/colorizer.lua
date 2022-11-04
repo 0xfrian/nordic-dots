d614cf79bc45245f29db978e7cf9ca0e496e5b65
0xfrian <0xfrian@gmail>
2022-11-04 15:43:51 -0700

    Starting from scratch

--- nvim/lua/settings/colorizer.lua
+++
@@ -1,6 +0,0 @@
-require("colorizer").setup({
-    "*"; 
-    markdown = {
-    names = false;
-    };
-})
--- picom/picom-launch.sh
+++
@@ -1,8 +0,0 @@
-# picom-launch.sh
-
-# Close existing picom instances
-killall -q picom
-sleep 1
-
-# Start picom
-picom --experimental-backends &
